77a01fc0185bda72c4aea4c54d0f52e53e9d99ba
Prerna <>
2022-03-26 19:42:29 +0530

    Added data class for County Added main_activity layout file Added dimension, style  and value file Added live data to get country api

---
+++ app/src/main/java/com/example/draggerbasic/model/Country.kt
@@ -0,0 +1,13 @@
+package com.example.draggerbasic.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by PrernaSurbhi on 26/03/22.
+ */
+data class Country(
+    @SerializedName("name")
+    val countryName:String?,
+
+
+)
--- app/src/main/java/com/example/draggerbasic/viewModel/ListViewModel.kt
+++ app/src/main/java/com/example/draggerbasic/viewModel/ListViewModel.kt
@@ -1,9 +1,36 @@
 package com.example.draggerbasic.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.draggerbasic.model.Country
 
 /**
  * Created by PrernaSurbhi on 26/03/22.
  */
 class ListViewModel:ViewModel() {
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchCountries()
+    }
+
+    private fun fetchCountries(){
+        val mockData = listOf(
+            Country("countryA"),
+            Country("countryB"),
+            Country("countryC"),
+            Country("countryD"),
+            Country("countryE"),
+            Country("countryF"),
+            Country("countryG"),
+            Country("countryH"),
+            Country("countryI")
+        )
+
+        countryLoadError.postValue(false)
+        loading.postValue(false)
+        countries.postValue(mockData)
+    }
 }
